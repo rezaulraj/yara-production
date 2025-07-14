ac0d898e936e91c974f9d4939e6e07449eea76be
rezaul-karim <rezaul.coderpro@gmail.com>
2025-07-14 19:08:43 +0600

    update file commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    // Add these for better production builds
+    minify: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+  },
 });
